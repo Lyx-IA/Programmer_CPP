080b3724ab1f71ec19f211ac88c378713cd24141
Lyx-IA <c.gerges@ludus-academie.com>
2019-10-17 11:36:44 +0200

    no message

--- Programmer_CPP_Bases/Programmer_CPP_Bases/Main.cpp
+++ Programmer_CPP_Bases/Programmer_CPP_Bases/Main.cpp
@@ -1,43 +1,48 @@
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
 
-using namespace std;
+///'cout', 'cin' et 'endl' font partie du namespace 'std', il faut soit inclure le namespace en début de programme ou rajouter 'std::' devant les primitive de 'std'
+//using namespace std;
 
-//Exemple de programme en C ++
+///Exemple de programme en C ++
 
 
 
 int main()
 {
 	printf("Hello world!\n");
+
+	///Variables ou constantes locales
 	int i = 0;
 	float x = 0.0;
 	float racx = 0.0;
-
+	///Constantes
 	const int NFOIS = 5;
 
 	printf("Bonjour\n");
 
 
-
-	cout << "Je vais vous calculer" << NFOIS << "racines carrees\n";
+	///cout envoie une informatoin typé dans la console
+	std::cout << "Je vais vous calculer" << NFOIS << "racines carrees\n";
 
 	for (i = 0; i < NFOIS; i++) {
 
-		cout << "Donnez un nombre : ";
-		cin >> x;
+		std::cout << "Donnez un nombre : ";
+		///cin permet de saisir une information
+		std::cin >> x;
 
 
 		if (x < 0.0)
-
-			cout << "Le nombre" << x << "ne possede pas de racine carree\n";
+			std::cout << "Le nombre" << x << "ne possede pas de racine carree\n";
 		else {
 			racx = sqrtf(x);
-			cout << "Le nombre" << x << "a pour racine carree" << racx << endl;
+			///endl est un retour chariot
+			std::cout << "Le nombre " << x << " a pour racine carree " << racx << std::endl;
 		}
 	}
 
-	cout << "Travail Termine" << endl;
+	std::cout << "Travail Termine" << std::endl;
 
 
 	return 0;
